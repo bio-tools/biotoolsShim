86c605647197ceedc3222c1985418c1fb676066e
Jon Ison <jison@cbs.dtu.dk>
2016-12-08 23:18:14 +0000

    corrections to scripts to match file names in repo

--- xmlToFrameworkXML/DoTransformXSLT2.0.bat
+++ xmlToFrameworkXML/DoTransformXSLT2.0.bat
@@ -1,4 +1,4 @@
 @echo off
 
-RaptorXML xslt --xslt-version=2 --input="biotools-2.0.0.xml" --output="biotools-2.0.0_framework.xml" %* "MappingMapTobiotools-2_0_0_framework.xslt"
+RaptorXML xslt --xslt-version=2 --input="input.xml" --output="output.xml" %* "biotoolsSchema_2.0.0_XML_to_framework_XML_xslt2.0.xslt"
 IF ERRORLEVEL 1 EXIT/B %ERRORLEVEL%
