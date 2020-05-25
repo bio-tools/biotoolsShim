1c9801d8aa95f106a2a0a924e5c8408f57edd170
Jon Ison <jon.c.ison@gmail.com>
2020-05-25 16:50:00 +0100

    Deleting unwanted files

--- genericxml2xml/versions/SAFE/biotools-3.3.0/DoTransformXSLT2.0.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-RaptorXML xslt --xslt-version=2 --input="input.xml" --output="output.xml" %* "framework_XML_to_biotoolsSchema_3.0.0_XML_xslt2.0.xslt"
-IF ERRORLEVEL 1 EXIT/B %ERRORLEVEL%
